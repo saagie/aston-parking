f57a262d6645deb314995fd55f44c150d2bc8419
Pierre Leresteux <pierre.leresteux@gmail.com>
2017-08-30 18:35:35 +0200

    Add JBot and custom commands (/command and /register)

---
+++ src/main/kotlin/io/saagie/astonparking/slack/SlackBot.kt
@@ -0,0 +1,39 @@
+package io.saagie.astonparking.slack
+
+
+import me.ramswaroop.jbot.core.slack.Bot
+import me.ramswaroop.jbot.core.slack.Controller
+import me.ramswaroop.jbot.core.slack.EventType
+import me.ramswaroop.jbot.core.slack.models.Event
+import me.ramswaroop.jbot.core.slack.models.Message
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.WebSocketSession
+
+
+@Component
+class SlackBot : Bot() {
+
+    @Value("\${slack.botToken}")
+    private val slackToken: String? = null
+
+    override fun getSlackToken(): String? {
+        return slackToken
+    }
+
+    override fun getSlackBot(): Bot {
+        return this
+    }
+
+
+    @Controller(events = arrayOf(EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE))
+    fun onReceiveDM(session: WebSocketSession, event: Event) {
+        reply(session, event, Message(
+                """Hi, I am ${slackService.currentUser.name}
+                    |You can interact with me using commands.
+                    |To have all available commands, type : /command
+                """.trimMargin()))
+    }
+
+
+}
