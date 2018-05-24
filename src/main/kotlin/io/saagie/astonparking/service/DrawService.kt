b75752c0c6334c03f711e43389e701cba3265c2d
Pierre Leresteux <pierre.leresteux@gmail.com>
2018-05-24 14:08:03 +0200

    feat : Change message when release too late

--- src/main/kotlin/io/saagie/astonparking/service/DrawService.kt
+++ src/main/kotlin/io/saagie/astonparking/service/DrawService.kt
@@ -225,7 +225,7 @@ class DrawService(
     }
 
     @Async
-    fun release(userId: String, text: String) {
+    fun release(userId: String, text: String): Boolean {
         val date = extractDate(text)
         val user = userService.get(userId)
         val schedule = scheduleDao.findByDate(date)
@@ -242,6 +242,7 @@ class DrawService(
         if (!checkAndPickIfRequest(date)) {
             slackBot.spotRelease(date)
         }
+        return drawRules.checkIfDateIsOk(date)
     }
 
 
--- src/main/kotlin/io/saagie/astonparking/slack/SlackSlashCommand.kt
+++ src/main/kotlin/io/saagie/astonparking/slack/SlackSlashCommand.kt
@@ -410,11 +410,10 @@ class SlackSlashCommand(
             return Message("No luck man, You're not allowed to do that.")
 
         try {
-            drawService.extractDate(text)
-            drawService.release(userId, text)
-
-            val message = Message("You have release the spot for the day (${text}). Another can now pick it. Thanks.")
-            return message
+            if (!drawService.release(userId, text)){
+                return Message("You have release the spot for the day (${text}). *BUT* it's a bit late for anyone to pick it. You'll have a blame in the future. Next time keep in mind that's it's important to release earlier as possible ... Thanks")
+            }
+            return Message("You have release the spot for the day (${text}). Another can now pick it. Thanks.")
 
         } catch (iae: IllegalArgumentException) {
             return Message(iae.message)
